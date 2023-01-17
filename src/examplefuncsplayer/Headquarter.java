167c4ed5739f980e2157964a826e8ade010057e9
Rodrigo Ipince <ipince@users.noreply.github.com>
2023-01-16 19:12:25 -0500

    Move robot logic into its own files

---
+++ src/examplefuncsplayer/Headquarter.java
@@ -0,0 +1,43 @@
+package examplefuncsplayer;
+
+import battlecode.common.Anchor;
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+import battlecode.common.RobotType;
+
+public class Headquarter extends RobotPlayer {
+
+    static int READ_WINDOW = 10;
+
+    static int roundsSinceLastReading = 999999999;
+
+    /**
+     * Run a single turn for a Headquarters.
+     * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
+     */
+    public static void run(RobotController rc) throws GameActionException {
+        // Pick a direction to build in.
+        Direction dir = directions[rng.nextInt(directions.length)];
+        MapLocation newLoc = rc.getLocation().add(dir);
+        if (rc.canBuildAnchor(Anchor.STANDARD)) {
+            // If we can build an anchor do it!
+            rc.buildAnchor(Anchor.STANDARD);
+            rc.setIndicatorString("Building anchor!");
+        }
+        if (rng.nextBoolean()) {
+            // Let's try to build a carrier.
+            rc.setIndicatorString("Trying to build a carrier");
+            if (rc.canBuildRobot(RobotType.CARRIER, newLoc)) {
+                rc.buildRobot(RobotType.CARRIER, newLoc);
+            }
+        } else {
+            // Let's try to build a launcher.
+            rc.setIndicatorString("Trying to build a launcher");
+            if (rc.canBuildRobot(RobotType.LAUNCHER, newLoc)) {
+                rc.buildRobot(RobotType.LAUNCHER, newLoc);
+            }
+        }
+    }
+}
---
+++ src/examplefuncsplayer/Launcher.java
@@ -0,0 +1,33 @@
+package examplefuncsplayer;
+
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+import battlecode.common.RobotInfo;
+import battlecode.common.Team;
+
+public class Launcher extends RobotPlayer {
+
+    public static void run(RobotController rc) throws GameActionException {
+        // Try to attack someone
+        int radius = rc.getType().actionRadiusSquared;
+        Team opponent = rc.getTeam().opponent();
+        RobotInfo[] enemies = rc.senseNearbyRobots(radius, opponent);
+        if (enemies.length >= 0) {
+            // MapLocation toAttack = enemies[0].location;
+            MapLocation toAttack = rc.getLocation().add(Direction.EAST);
+
+            if (rc.canAttack(toAttack)) {
+                rc.setIndicatorString("Attacking");
+                rc.attack(toAttack);
+            }
+        }
+
+        // Also try to move randomly.
+        Direction dir = directions[rng.nextInt(directions.length)];
+        if (rc.canMove(dir)) {
+            rc.move(dir);
+        }
+    }
+}
